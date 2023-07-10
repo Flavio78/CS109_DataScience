161b0198425d0ba1d71690a89d7e44e53c13bcca
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-10 23:13:38 +0200

    test environment

--- lectureA/env0.py
+++ lectureA/env0.py
@@ -1,13 +1,17 @@
+"""Setup file for testing Python environment"""
 import numpy
-import cowsay
+# import cowsay
 import requests
 import pytest
 
-def main():
-    print(f"cowsay version: {cowsay.__version__}")
+
+def main() -> None:
+    """Main function"""
+    # print(f"cowsay version: {cowsay.__version__}")
     print(f"numpy version: {numpy.__version__}")
     print(f"pytest version: {pytest.__version__}")
     print(f"requests version: {requests.__version__}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
